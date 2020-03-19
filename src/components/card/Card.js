5df20ea742a7bee15605068d9d99c21151ffdcae
Duncan Williams <duncanlw@gmail.com>
2020-03-19 23:34:53 +0200

    updated font size

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -12,31 +12,53 @@ const CardContainer = props => {
         className="justify-content-center"
       >
         <Card.Header as="h3">{props.title}</Card.Header>
-        <Card.Body>
+        <Card.Body style={{ fontSize: "17px" }}>
           {props.selection[0] ? (
-            <Card.Text>Cases: {props.cases}</Card.Text>
+            <Card.Text>
+              <strong>Cases: </strong>
+              {props.cases}
+            </Card.Text>
           ) : null}
           {props.selection[1] ? (
-            <Card.Text>Deaths: {props.deaths}</Card.Text>
+            <Card.Text>
+              <strong>Deaths: </strong>
+              {props.deaths}
+            </Card.Text>
           ) : null}
           {props.selection[2] ? (
-            <Card.Text>Recoveries: {props.recoveries}</Card.Text>
+            <Card.Text>
+              <strong>Recoveries: </strong>
+              {props.recoveries}
+            </Card.Text>
           ) : null}
           {props.selection[3] ? (
-            <Card.Text>Death rate: {deathRate.toFixed(1)}%</Card.Text>
+            <Card.Text>
+              <strong>Death rate: </strong>
+              {deathRate.toFixed(1)}%
+            </Card.Text>
           ) : null}
           {props.selection[4] ? (
-            <Card.Text>New cases today: {props.todayCases} </Card.Text>
+            <Card.Text>
+              <strong>New cases today: </strong>
+              {props.todayCases}{" "}
+            </Card.Text>
           ) : null}
           {props.selection[5] ? (
-            <Card.Text>Deaths today: {props.todayDeaths} </Card.Text>
+            <Card.Text>
+              <strong>Deaths today: </strong>
+              {props.todayDeaths}{" "}
+            </Card.Text>
           ) : null}
           {props.selection[6] ? (
-            <Card.Text>Critical condition: {props.critical} </Card.Text>
+            <Card.Text>
+              <strong>Critical condition: </strong>
+              {props.critical}{" "}
+            </Card.Text>
           ) : null}
           {props.selection[7] ? (
             <Card.Text>
-              Cases per million people: {props.casesPerOneMillion}
+              <strong>Cases per million people: </strong>
+              {props.casesPerOneMillion}
             </Card.Text>
           ) : null}
         </Card.Body>
