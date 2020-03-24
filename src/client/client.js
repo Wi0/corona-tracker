40e05494383295d034b60c737541d21b037c95dc
Duncan Williams <duncanlw@gmail.com>
2020-03-24 21:20:18 +0200

    Started using template literals

--- src/client/client.js
+++ src/client/client.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 const API_COUNTRIES = "https://corona.lmao.ninja/countries";
 const API_TOTAL = "https://corona.lmao.ninja/all";
-const API_HISTORY_BASE = "https://api.covid19api.com/total/dayone/country/";
-let API_HISTORY_COUNTRY = "united-kingdom";
-const API_HISTORY_ENDING = "/status/confirmed";
 const API_COUNTRY_LIST = "https://api.covid19api.com/countries";
 
 export const getCountryData = () => {
@@ -15,9 +12,7 @@ export const getTotalData = () => {
 };
 
 export const getHistoryData = country => {
-  API_HISTORY_COUNTRY = country;
-  const API_HISTORY =
-    API_HISTORY_BASE + API_HISTORY_COUNTRY + API_HISTORY_ENDING;
+  const API_HISTORY = `https://api.covid19api.com/total/dayone/country/${country}/status/confirmed`;
   return axios.get(API_HISTORY);
 };
 
