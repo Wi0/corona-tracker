5ea47c7e3ba4249a6e89089a43c19f06c33bc8f8
Duncan Williams <duncanlw@gmail.com>
2020-03-26 22:14:23 +0200

    added redirects

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\dunca\\Desktop\\WebDev\\react-projects\\corona-tracker\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\dunca\\Desktop\\WebDev\\react-projects\\corona-tracker\\src\\pages\\404.js"))),
+  "component---src-pages-history-js": hot(preferDefault(require("C:\\Users\\dunca\\Desktop\\WebDev\\react-projects\\corona-tracker\\src\\pages\\history.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\dunca\\Desktop\\WebDev\\react-projects\\corona-tracker\\src\\pages\\index.js")))
+}
+
